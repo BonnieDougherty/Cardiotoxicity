d46e8ca96e8d523f1839b3aed5825418031e2190
Dougherty <bvd5nq@virginia.edu>
2021-02-14 11:38:33 -0500

    initial commit

---
+++ code/MATLAB/RunTIDEs_cardiotoxicity.m
@@ -0,0 +1,215 @@
+% Testing various data sets with TIDEs
+clear all
+clc
+
+% initialize the toolbox
+initCobraToolbox
+changeCobraSolver('ibm_cplex', 'all');
+
+% Load in the heart model
+load('iRno_heart.mat')
+
+model = rno_heart_model;
+
+% Generate a taskStructure for determining the min set of reactions
+% Convert from original format to COBRA format
+inputFile = ['AllTasks_CardiomyocyteSpecific_RAT_COBRA.xlsx'];
+taskStructure=generateTaskStructure_BVD(inputFile);
+
+% calculate the reactions necessary for each task
+removeNoGPR = 'true';
+minRxnList = generateMinRxnList(model, taskStructure, removeNoGPR);
+
+% Add on analysis by reaction subsystem
+% Find a unique list of subsystems in this model
+reactions = {};
+for k = 1:length(rno_heart_model.subSystems)
+    reactions{k,1} = char(model.subSystems{k});
+end
+subsystem = unique(reactions);
+subsystem(1,:) = [];
+
+for k = 1:length(subsystem)
+    minRxnList(end+1).id = strcat('S',num2str(k));
+    minRxnList(end).description = subsystem{k};
+    minRxnList(end).rxns = model.rxns(strcmp(subsystem{k}, reactions));
+end
+
+% Remove reactions from the subsystem list that don't have GPR rules
+for k = 1:length(model.rxns)
+    noGPR(k) = isempty(model.grRules{k});
+end
+noGPR = model.rxns(noGPR);
+
+% Remove these reactions from the list of total_carrying_rxns
+for task = 1:length(minRxnList)
+    common = intersect(noGPR, minRxnList(task).rxns);
+    minRxnList(task).rxns = setdiff(minRxnList(task).rxns, common);
+end
+
+% Remove tasks that no longer have associated reactions
+task = 1;
+while task ~= length(minRxnList)
+    if length(minRxnList(task).rxns) < 3
+        minRxnList(task) = [];
+    else
+        task = task+1;
+    end
+end
+
+% Specify the number of random iterations of data
+parpool(4);
+num_iterations = 1000;
+
+%% Writing data to a file for enricher() function in R
+ID = [];
+reactions = [];
+for task = 1:length(minRxnList)
+    ID = [ID;repmat(string(minRxnList(task).id), length(minRxnList(task).rxns),1)];
+    reactions = [reactions; minRxnList(task).rxns];
+end
+
+temp = table(ID, reactions);
+writetable(temp, 'C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\data\TIDEs_rxns.csv', 'Delimiter', ',')
+
+%% Debugging reading in table problems
+opts = detectImportOptions('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\DEGs\dougherty_rno_cardio_t24_5fu_gene_deseq2.csv');
+opts.Delimiter = {','};
+
+%% Run TIDEs analysis for cardiotoxicity data
+% Load in data that had data for all genes, not just genes that are DEGs
+% Read in data from RNA-seq analysis
+Dox6hrs = readtable('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\RNA-seq DEG data\dougherty_rno_cardio_t6_Dox_6h_gene_deseq2_01fdr.csv', opts);
+Dox24hrs = readtable('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\RNA-seq DEG data\dougherty_rno_cardio_t6_Dox_24h_gene_deseq2_01fdr.csv', opts);
+
+Ace6hrs = readtable('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\RNA-seq DEG data\dougherty_rno_cardio_t6_Ace_6h_gene_deseq2_01fdr.csv', opts);
+Ace24hrs = readtable('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\RNA-seq DEG data\dougherty_rno_cardio_t6_Ace_24h_gene_deseq2_01fdr.csv', opts);
+
+FiveFU6hrs = readtable('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\RNA-seq DEG data\dougherty_rno_cardio_t6_5FU_6h_gene_deseq2_01fdr.csv', opts);
+FiveFU24hrs = readtable('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\RNA-seq DEG data\dougherty_rno_cardio_t6_5FU_24h_gene_deseq2_01fdr.csv', opts);
+
+%% Run test expression mapping to make sure EntrezIDs are formatted correctly
+data.gene = Dox6hrs.EntrezID;
+data.value = Dox6hrs.logfc;
+[expressionRxns parsedGPR] = mapExpressionToReactions(model, data);
+%%
+% Run Dox6 hrs analysis
+data.gene = Dox6hrs.EntrezID;
+data.value = Dox6hrs.logfc;
+[Dox6hrs, Dox6hrs_random] = calculateTIDEscores(model, minRxnList, data, num_iterations);
+
+% Run Dox24 hrs analysis
+data.gene = Dox24hrs.EntrezID;
+data.value = Dox24hrs.logfc;
+[Dox24hrs, Dox24hrs_random] = calculateTIDEscores(model, minRxnList, data, num_iterations);
+
+% Run Ace6 hrs analysis
+data.gene = Ace6hrs.EntrezID;
+data.value = Ace6hrs.logfc;
+[Ace6hrs, Ace6hrs_random] = calculateTIDEscores(model, minRxnList, data, num_iterations);
+
+% Run Ace24 hrs analysis
+data.gene = Ace24hrs.EntrezID;
+data.value = Ace24hrs.logfc;
+[Ace24hrs, Ace24hrs_random] = calculateTIDEscores(model, minRxnList, data, num_iterations);
+
+% Run Five5FU 6 hrs analysis
+data.gene = FiveFU6hrs.EntrezID;
+data.value = FiveFU6hrs.logfc;
+[FiveFU6hrs, FiveFU6hrs_random] = calculateTIDEscores(model, minRxnList, data, num_iterations);
+
+% Run Five5FU 24 hrs analysis
+data.gene = FiveFU24hrs.EntrezID;
+data.value = FiveFU24hrs.logfc;
+[Five5FU24hrs, Five5FU24hrs_random] = calculateTIDEscores(model, minRxnList, data, num_iterations);
+
+%% Save all the datasets to Excel spreadsheets
+data = Dox6hrs;
+% Save individual variable names to xlsx files in R/data folder
+data_save = {};
+for k = 1:length(data)
+    data_save{k,2} = data(k).description;
+    data_save{k,1} = data(k).id;
+    data_save{k,4} = data(k).significance;
+    data_save{k,3} = data(k).taskScore;
+end
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Dox6hrs_01fdr.xlsx', data_save)
+
+data = Dox24hrs;
+% Save individual variable names to xlsx files in R/data folder
+data_save = {};
+for k = 1:length(data)
+    data_save{k,2} = data(k).description;
+    data_save{k,1} = data(k).id;
+    data_save{k,4} = data(k).significance;
+    data_save{k,3} = data(k).taskScore;
+end
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Dox24hrs_01fdr.xlsx', data_save)
+
+%% Ace data
+data = Ace6hrs;
+% Save individual variable names to xlsx files in R/data folder
+data_save = {};
+for k = 1:length(data)
+    data_save{k,2} = data(k).description;
+    data_save{k,1} = data(k).id;
+    data_save{k,4} = data(k).significance;
+    data_save{k,3} = data(k).taskScore;
+end
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Ace6hrs_01fdr.xlsx', data_save)
+
+data = Ace24hrs;
+% Save individual variable names to xlsx files in R/data folder
+data_save = {};
+for k = 1:length(data)
+    data_save{k,2} = data(k).description;
+    data_save{k,1} = data(k).id;
+    data_save{k,4} = data(k).significance;
+    data_save{k,3} = data(k).taskScore;
+end
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Ace24hrs_01fdr.xlsx', data_save)
+
+%% FiveFU
+data = FiveFU6hrs;
+% Save individual variable names to xlsx files in R/data folder
+data_save = {};
+for k = 1:length(data)
+    data_save{k,2} = data(k).description;
+    data_save{k,1} = data(k).id;
+    data_save{k,4} = data(k).significance;
+    data_save{k,3} = data(k).taskScore;
+end
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\FiveFU6hrs_01fdr.xlsx', data_save)
+
+data = Five5FU24hrs;
+% Save individual variable names to xlsx files in R/data folder
+data_save = {};
+for k = 1:length(data)
+    data_save{k,2} = data(k).description;
+    data_save{k,1} = data(k).id;
+    data_save{k,4} = data(k).significance;
+    data_save{k,3} = data(k).taskScore;
+end
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\FiveFU24hrs_01fdr.xlsx', data_save)
+
+%% Save the random data sets
+data = Ace6hrs_random; 
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Ace6hrs_random_01fdr.xlsx', data);
+
+data = Dox24hrs_random;
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Dox24hrs_random_01fdr.xlsx', data);
+
+data = Ace24hrs_random; 
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Ace24hrs_random_01fdr.xlsx', data);
+
+data = Dox6hrs_random;
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\Dox6hrs_random_01fdr.xlsx', data);
+
+data = FiveFU6hrs_random; 
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\5FU6hrs_random_01fdr.xlsx', data);
+
+data = Five5FU24hrs_random;
+xlswrite('C:\Users\bvd5nq\Documents\R scripts\Analyzing RNA-seq data\TIDEs\5FU24hrs_random_01fdr.xlsx', data);
+
+%% Save the results to a MATLAB file
+save('cardiotoxicity.mat','Ace24hrs','Ace6hrs','Dox24hrs','Dox6hrs','FiveFU24hrs','FiveFU6hrs')
---
+++ code/MATLAB/iRno_heart_model.m
@@ -0,0 +1,185 @@
+%% Generating rat iCardio model from iRno and iCardio
+clear all
+close all
+clc
+
+% Load the COBRA toolbox
+initCobraToolbox
+changeCobraSolver('ibm_cplex', 'all');
+
+% Loading the iRno COBRA file
+rno_cobra_load = ncomm_blais_xls2model('data/ncomms14250-s12, iRno COBRA.xlsx');
+% Change objective function for ATP synthesis
+rno_cobra_load = changeObjective(rno_cobra_load, 'RCR11017');
+
+% Loading the iHsa COBRA file
+hsa_cobra_load = ncomm_blais_xls2model('data/ncomms14250-s10, iHsa COBRA.xlsx');
+% Change objective function for ATP synthesis
+hsa_cobra_load = changeObjective(hsa_cobra_load, 'RCR11017');
+
+% Make changes to the original reconstruction based off new metabolic tasks
+% 1: Change the DHAP reaction to be reveresed
+f = findRxnIDs(rno_cobra_load,'RCR21050');
+rno_cobra_load.lb(f) = -1000;
+rno_cobra_load.ub(f) = 0;
+% Make the same changes for hsa_cobra_load
+f = findRxnIDs(hsa_cobra_load,'RCR21050');
+hsa_cobra_load.lb(f) = -1000;
+hsa_cobra_load.ub(f) = 0;
+
+% 2: Remove superoxide movement from the model (currently just exported out)
+f = findRxnIDs(rno_cobra_load,'RCR40428');
+rno_cobra_load.lb(f) = 0;
+rno_cobra_load.ub(f) = 0;
+% Make the same changes for hsa_cobra_load
+f = findRxnIDs(hsa_cobra_load,'RCR40428');
+hsa_cobra_load.lb(f) = 0;
+hsa_cobra_load.ub(f) = 0;
+
+% 3: All catalase reactions have bounds of [0,0] change to [0,1000]
+rxns = findRxnIDs(rno_cobra_load, {'RCR10165' 'RCR10607' 'RCR11007' 'RCR11029' 'RCR14124'});
+rno_cobra_load.lb(rxns) = 0;
+rno_cobra_load.ub(rxns) = 1000;
+% Make the same changes for hsa_cobra_load
+hsa_cobra_load.lb(rxns) = 0;
+hsa_cobra_load.ub(rxns) = 1000;
+
+% 4: Change the Complex I reaction to include superoxide production
+rno_cobra_load = addReaction(rno_cobra_load, 'RCR21048', {'m03103[m]','m02553[m]','m02039[m]', 'm02630[m]', 'm03102[m]','m02552[m]','m02039[c]','m02631[m]'}, [-1 -1 -5 -0.04 1 1 4 0.04], false);
+hsa_cobra_load = addReaction(hsa_cobra_load, 'RCR21048', {'m03103[m]','m02553[m]','m02039[m]', 'm02630[m]', 'm03102[m]','m02552[m]','m02039[c]','m02631[m]'}, [-1 -1 -5 -0.04 1 1 4 0.04], false);
+
+% 5: Change the number of protons moved to generate one ATP
+rno_cobra_load = addReaction(rno_cobra_load, 'RCR20085', {'m02751[m]','m01285[m]','m02039[c]', 'm01371[m]', 'm02039[m]','m02040[m]'}, [-1 -1 -2.7 1 2.7 1], true);
+hsa_cobra_load = addReaction(hsa_cobra_load, 'RCR20085', {'m02751[m]','m01285[m]','m02039[c]', 'm01371[m]', 'm02039[m]','m02040[m]'}, [-1 -1 -2.7 1 2.7 1], true);
+
+% 6: Change all exchange reaction bounds that are not 1000, 0, or -1000
+lbx = find(rno_cobra_load.lb ~= -1000 & rno_cobra_load.lb ~= 0);
+rno_cobra_load.lb(lbx) = -1000;
+ubx = find(rno_cobra_load.ub ~= 1000 & rno_cobra_load.ub ~= 0);
+rno_cobra_load.ub(ubx) = 1000;
+% Make the same changes for the iHsa model
+lbx = find(hsa_cobra_load.lb ~= -1000 & hsa_cobra_load.lb ~= 0);
+hsa_cobra_load.lb(lbx) = -1000;
+ubx = find(hsa_cobra_load.ub ~= 1000 & hsa_cobra_load.ub ~= 0);
+hsa_cobra_load.ub(ubx) = 1000;
+
+%% Compare iRno to iCardio to develop rat-specific cardiac model
+% Load in the heart model
+% stored as the variable heart_model_curation
+load('data/HeartModel.mat')
+
+% deleted contains rxn_ids for reactions removed from the model
+deleted = [];
+for i = 1:length(hsa_cobra_load.rxns)
+    temp = strcmp(hsa_cobra_load.rxns{i}, heart_model_curation.rxns());
+    if max(temp) == 0
+        deleted(end+1,1) = i;
+    end
+end
+
+% Re-create heart model based off new deleted reaction list
+deleted_rxns = {};
+for k = 1:length(deleted)
+    deleted_rxns{end+1} = hsa_cobra_load.rxns{deleted(k)};
+end
+
+rno_heart_model = removeRxns(rno_cobra_load, deleted_rxns);
+hsa_heart_model = removeRxns(hsa_cobra_load, deleted_rxns);
+
+% Remove gluconeogenesis from the model through transport of glucose out of
+% the model
+temp = findRxnIDs(rno_heart_model,'RCR40464');
+rno_heart_model.ub(temp) = 0;
+
+%% Check metabolic tasks with the new rat heart model
+model = rno_heart_model;
+
+% Check completion of metabolic tasks from iCardio
+inputFile = ['data/AllTasks_CardiomyocyteSpecific_RAT.xlsx'];
+[FINAL] = generateCobraTaskList_new(inputFile, rno_cobra_load);
+xlswrite('data/AllTasks_CardiomyocyteSpecific_RAT_COBRA.xlsx', FINAL, 'TASKS')
+inputFile = ['data/AllTasks_CardiomyocyteSpecific_RAT_COBRA.xlsx'];
+
+rno_tasks = checkMetabolicTasks_BVD(model,inputFile);
+
+%% Look at the reactions that are in iRno that aren't in iHsa
+% All reactions are in both models, bounds are different
+
+% 72 reactions are disabled in iHsa
+iHsa_removed = [];
+for rxn = 1:length(hsa_cobra_load.rxns)
+    if(hsa_cobra_load.lb(rxn) == 0 && hsa_cobra_load.ub(rxn) == 0)
+        iHsa_removed(end+1,1) = rxn;
+    end
+end
+
+% Only 61 reactions disabled in iRno
+iRno_removed = [];
+for rxn = 1:length(rno_cobra_load.rxns)
+    if(rno_cobra_load.lb(rxn) == 0 && rno_cobra_load.ub(rxn) == 0)
+        iRno_removed(end+1,1) = rxn;
+    end
+end
+
+iRno_removed = rno_cobra_load.rxns(iRno_removed);
+iHsa_removed = hsa_cobra_load.rxns(iHsa_removed);
+
+% Compare to see the 11 reactions that are different
+for rxn = 1:length(iHsa_removed)
+    if sum(strcmp(iHsa_removed{rxn}, iRno_removed)) == 0
+        iHsa_removed{rxn}
+    end
+end
+
+%% Reactions to be added based on NCBI evidence
+add_rxns = {
+    'RCR10020'
+    'RCR90016'
+    'RCR90127'
+    'RCR90128'
+    'RCR90129'
+    'RCR90135'
+    'RCR90145'
+    'RCR90148'
+    % no evidence of expression in the heart but included all reactions
+    % associated with this GPR
+    'RCR10559'
+    'RCR11533'
+    'RCR14431'
+    'RCR90004'
+    'RCR90005'};
+
+% deleted contains rxn_ids for reactions removed from the model
+deleted = [];
+for i = 1:length(rno_cobra_load.rxns)
+    temp = strcmp(rno_cobra_load.rxns{i}, rno_heart_model.rxns());
+    if max(temp) == 0
+        deleted(end+1,1) = i;
+    end
+end
+
+deleted_update = deleted;
+for i = 1:length(add_rxns)
+    [trash rxn_ID] = max(strcmp(add_rxns{i}, rno_cobra_load.rxns()));
+    location = find(deleted_update == rxn_ID);
+    deleted_update(location) = [];
+end
+
+% Re-create heart model based off new deleted reaction list
+deleted_rxns = {};
+for k = 1:length(deleted_update)
+    deleted_rxns{end+1} = rno_cobra_load.rxns{deleted_update(k)};
+end
+
+rno_heart_model = removeRxns(rno_cobra_load, deleted_rxns);
+
+% Remove gluconeogenesis from the model through transport of glucose out of
+% the model
+temp = findRxnIDs(rno_heart_model,'RCR40464');
+rno_heart_model.ub(temp) = 0;
+
+%% Re-check metabolic tasks
+model = rno_heart_model;
+rno_tasks = checkMetabolicTasks_BVD(model,inputFile);
+%% Save the model to a new MATLAB file
+save('data/iRno_heart.mat', 'rno_heart_model', 'rno_cobra_load')
